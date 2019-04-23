0fe886436680e21026e9fef83e33490ff5c39805
Walter Vetrivel <waltervetrivel@yahoo.in>
2019-04-23 22:53:31 +1000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {PageHeader, Form, Icon, Button, Input} from 'antd';
+import RegistrationForm from './Registration';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		events: []
+	};
+
+	async componentDidMount() {
+		const results = await axios({
+			method: 'POST',
+			url: 'http://localhost:4000',
+			data: {
+				query: `
+				{
+					users {
+						firstName
+					}
+				}`
+			}
+		});
+		console.log(results);
+	}
+
+	onSubmit = e => {
+		e.preventDefault();
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<PageHeader
+					backIcon={false}
+					title="Event Booker"
+					subTitle="Booking events made easy">
+					<Form layout="inline" onSubmit={this.onSubmit.bind(this)}>
+						<Form.Item>
+							<Input prefix={<Icon type="user" />} placeholder="Username" />
+						</Form.Item>
+						<Form.Item>
+							<Input prefix={<Icon type="lock" />} placeholder="Password" />
+						</Form.Item>
+						<Form.Item>
+							<Button type="primary" htmlType="submit">
+								Log in
+							</Button>
+						</Form.Item>
+					</Form>
+				</PageHeader>
+				<RegistrationForm />
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/Registration.js
@@ -0,0 +1,144 @@
+import React, {Component} from 'react';
+import {Form, Button, Input, Row, Col, DatePicker} from 'antd';
+import moment from 'moment';
+import axios from 'axios';
+
+class Registration extends Component {
+	state = {
+		firstName: '',
+		lastName: '',
+		dateOfBirth: moment(new Date(), 'YYYY - MM - DD'),
+		email: '',
+		location: ''
+	};
+
+	onFirstNameChange = e => {
+		this.setState({firstName: e.target.value});
+	};
+
+	onLastNameChange = e => {
+		this.setState({lastName: e.target.value});
+	};
+
+	onEmailChange = e => {
+		this.setState({email: e.target.value});
+	};
+
+	onPasswordChange = e => {
+		this.setState({password: e.target.value});
+	};
+
+	onLocationChange = e => {
+		this.setState({location: e.target.value});
+	};
+
+	onDateOfBirthChange = e => {
+		if (e) {
+			console.log(new Date(e._i).toISOString());
+			this.setState({dateOfBirth: e});
+		}
+	};
+
+	onSubmit = async e => {
+		e.preventDefault();
+		this.props.form.validateFields((err, values) => {
+			if (!err) {
+				console.log('Received values of form: ', values);
+			}
+		});
+		console.log(this.state);
+		const mutationString = `mutation {
+			createUser(data: {
+				firstName: "${this.state.firstName}"
+				lastName: "${this.state.lastName}"
+				dateOfBirth: "${new Date(this.state.dateOfBirth._i).toISOString()}"
+				email: "${this.state.email}"
+				password: "${this.state.password}"
+				location: "${this.state.location}"
+			}) {
+				id
+				firstName
+				lastName
+			}
+		}`;
+		const result = await axios({
+			method: 'POST',
+			url: 'http://localhost:4000',
+			data: {
+				query: mutationString
+			}
+		});
+		console.log(result);
+	};
+
+	render() {
+		return (
+			<Row type="flex" justify="center">
+				<Col span={12}>
+					<h1>Register</h1>
+					<Form
+						labelCol={{xs: {span: 24}, sm: {span: 8}}}
+						wrapperCol={{xs: {span: 24}, sm: {span: 16}}}
+						onSubmit={this.onSubmit}>
+						<Form.Item label="First Name">
+							<Input
+								placeholder="Eg. John"
+								value={this.state.firstName}
+								onChange={this.onFirstNameChange}
+							/>
+						</Form.Item>
+						<Form.Item label="Last Name">
+							<Input
+								placeholder="Eg. Doe"
+								value={this.state.lastName}
+								onChange={this.onLastNameChange}
+							/>
+						</Form.Item>
+						<Form.Item label="Email">
+							<Input
+								type="email"
+								placeholder="Eg. example@example.com"
+								value={this.state.email}
+								onChange={this.onEmailChange}
+							/>
+						</Form.Item>
+						<Form.Item label="Password">
+							<Input
+								type="password"
+								placeholder="Your password here"
+								value={this.state.password}
+								onChange={this.onPasswordChange}
+							/>
+						</Form.Item>
+						<Form.Item label="Location">
+							<Input
+								placeholder="Eg. Australia"
+								value={this.state.location}
+								onChange={this.onLocationChange}
+							/>
+						</Form.Item>
+						<Form.Item label="Date of Birth">
+							<DatePicker
+								value={this.state.dateOfBirth}
+								onChange={this.onDateOfBirthChange}
+							/>
+						</Form.Item>
+						<Row type="flex" justify="end">
+							<Form.Item>
+								<Button htmlType="submit" type="primary">
+									Register
+								</Button>
+							</Form.Item>
+						</Row>
+					</Form>
+				</Col>
+			</Row>
+		);
+	}
+}
+
+const WrappedRegistrationForm = Form.create({name: 'registration'})(
+	Registration
+);
+
+export default WrappedRegistrationForm;
